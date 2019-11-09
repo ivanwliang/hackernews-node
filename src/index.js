71d24e63e44003021eef8542efbf9a7adeaaa269
Ivan Liang <ivanwenliang@gmail.com>
2019-11-08 17:28:28 -0800

    Attach generated prisma client to context

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const { GraphQLServer } = require("graphql-yoga");
+const { prisma } = require("./generated/prisma-client");
 
 const resolvers = {
   Query: {
@@ -19,6 +20,9 @@ const resolvers = {
 
 const server = new GraphQLServer({
   typeDefs: "./src/schema.graphql",
-  resolvers
+  resolvers,
+  context: {
+    prisma
+  }
 });
 server.start(() => console.log(`Server is running on http://localhost:4000`));
